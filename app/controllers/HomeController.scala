969342f40e700103c773cb40b9fe13484c5720e7
georgeherby <georgeherby@hotmail.com>
2018-06-22 21:40:14 +0100

    Beautifying!

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.util.Date
+import java.util.Locale
 
 import javax.inject._
 import play.api.mvc._
@@ -10,43 +10,37 @@ import play.api.Configuration
 import play.api.libs.json._
 import scalaj.http.Http
 import models.PRDetails
+import play.api.i18n._
 
 import scala.collection.mutable.ArrayBuffer
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
-@Singleton
-class HomeController @Inject()(config: Configuration, cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+@Singleton
+class HomeController @Inject()(config: Configuration, cc: ControllerComponents, messagesApi: MessagesApi) extends AbstractController(cc)  {
+
   def index = Action {
 
-    val request = Http(s"https://api.github.com/teams/${config.get[Int]("github.team_id")}/repos?per_page=1000")
+    val lang: Lang = new Lang(Locale.ENGLISH)
+    val messages: Messages = MessagesImpl(lang, messagesApi)
+
+
+    val getReposForTeamRequest = Http(s"https://api.github.com/teams/${config.get[Int]("github.team_id")}/repos?per_page=1000")
       .header("Accept", "application/vnd.github.hellcat-preview+json")
       .header("Authorization", s"token ${config.get[String]("github.accesstoken")}").asString
 
-    val req = request.body
+    val getReposForTeamJson = Json.parse(getReposForTeamRequest.body)
 
-    val jsn = Json.parse(req)
-
-    val noOfArrays = jsn.as[JsArray].value.size
+    val noOfTeams = getReposForTeamJson.as[JsArray].value.size
 
     val arrayBuffer = ArrayBuffer[PRDetails]()
 
-    for (item <- jsn.as[JsArray].value){
-      val repoName = (item \ "name").as[String]
-      val request2 = Http(s"https://api.github.com/repos/hmrc/$repoName/pulls")
+    for (repo <- getReposForTeamJson.as[JsArray].value){
+      val repoName = (repo \ "name").as[String]
+      val getRepoPrsRequest = Http(s"https://api.github.com/repos/hmrc/$repoName/pulls")
         .header("Accept", "application/vnd.github.symmetra-preview+json")
         .header("Authorization", s"token ${config.get[String]("github.accesstoken")}").asString
 
-      val prs = Json.parse(request2.body).as[JsArray].value
+      val prs = Json.parse(getRepoPrsRequest.body).as[JsArray].value
 
       if (prs.nonEmpty){
         for (pr <- prs){
@@ -65,7 +59,7 @@ class HomeController @Inject()(config: Configuration, cc: ControllerComponents) 
         }
       }
     }
-    Ok(views.html.index(noOfArrays,arrayBuffer.sortWith(_.repo < _.repo)))
+    Ok(views.html.index(config.get[String]("team_name"), noOfTeams,arrayBuffer.sortWith(_.repo < _.repo), messages))
   }
 
   def getCleanDate(date: String): String = {
